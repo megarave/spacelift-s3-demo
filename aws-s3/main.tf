6ce02411759f18eedb1af94fa49cd77fb14c6731
megarave <168197854+megarave@users.noreply.github.com>
2026-06-01 17:49:44 +0300

    feat: add second S3 bucket resource

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -1,19 +1,39 @@
 resource "random_string" "random" {
-  length = 10
+  length  = 10
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "random_integer" "random" {
-    min = 1
-    max = 10000
+  min = 1
+  max = 10000
 }
 
 resource "aws_s3_bucket" "this" {
-    bucket = "random-bucket-${random_string.random.result}-${random_integer.random.result}"
+  bucket = "random-bucket-${random_string.random.result}-${random_integer.random.result}"
 
-    tags = {
-      ManagedBy   = "spacelift"
-      Environment = "demo"
-    }
+  tags = {
+    ManagedBy   = "spacelift"
+    Environment = "demo"
+  }
+}
+
+resource "random_string" "random2" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "random_integer" "random2" {
+  min = 1
+  max = 10000
+}
+
+resource "aws_s3_bucket" "second" {
+  bucket = "random-bucket-${random_string.random2.result}-${random_integer.random2.result}"
+
+  tags = {
+    ManagedBy   = "spacelift"
+    Environment = "demo"
+  }
 }
